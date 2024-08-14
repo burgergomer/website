1263f76858185d63d8c755d279772c2fb31a341c
burgergomer <albyace88@gmail.com>
2024-08-14 13:02:05 +1000

    title

--- index.php
+++ index.php
@@ -5,9 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="style.css" rel="stylesheet" />
-<script src="javascript.js"></script>
+    <script src="javascript.js"></script>
 </head>
 <body>
-    
+    <h1>
+    Welcome Silly Monkeys :D
+    </h1>
 </body>
 </html>
